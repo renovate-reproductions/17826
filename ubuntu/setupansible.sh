2fe700d919367bbf955e55efb0b4df2f0e1b557d
Pat Heard <patrick.heard@canada.ca>
2019-09-23 11:50:54 -0400

    Install ansible-lint as part of setup

--- ubuntu/setupansible.sh
+++ ubuntu/setupansible.sh
@@ -7,4 +7,5 @@ apt-get install software-properties-common -y &&
 apt-add-repository ppa:ansible/ansible -y &&
 apt-get update && 
 apt-get install ansible -y &&
+apt-get install ansible-lint -y &&
 ansible-playbook test.yml --connection=local
