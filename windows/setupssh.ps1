5abaeefe73191bd4a8204cd43f53b77e35bd9d47
Pat Heard <patrick.heard@canada.ca>
2019-09-23 16:15:55 -0400

    Add sshd_config public key auth to script

--- windows/setupssh.ps1
+++ windows/setupssh.ps1
@@ -7,7 +7,14 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object
 # Set ssh to use `powershell` as its shell
 Set-Itemproperty -path 'HKLM:\SOFTWARE\OpenSSH' -Name 'DefaultShell' -value 'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe'
 
-# TODO: set `PubkeyAuthentication yes` in %programdata%\ssh\sshd_config
+# Enable public key auth with SSH
+$sshd_conf_path = "$env:programdata\ssh\sshd_config"
+$sshd_conf = (Get-Content $Path)
+$sshd_conf = $sshd_conf -Replace "Match Group administrators", "#Match Group administrators"
+$sshd_conf = $sshd_conf -Replace "AuthorizedKeysFile __PROGRAMDATA__/ssh/administrators_authorized_keys", "#AuthorizedKeysFile __PROGRAMDATA__/ssh/administrators_authorized_keys"
+Set-Content  -Path $sshd_conf_path -Value $sshd_conf
+Add-Content  -Path $sshd_conf_path -Value "`nPubkeyAuthentication yes"
+
 # TODO: `cat` control node's id_rsa.pub into %userprofile%\.ssh\authorized_keys
 
 # Restart ssh service
